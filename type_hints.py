33e10419e324f86a8ffdd56b395abcd4dd83acd9
Arminder <armincheema@gmail.com>
2026-05-26 20:35:27 +0800

    Changed

--- type_hints.py
+++ type_hints.py
@@ -1,12 +1,28 @@
+
+"""
+
+Docstring is used to document a function, a module or a class
+
 #type hints are run by mypy before compling with python complier to cather the type error 
 #python is interpreted not compiled
+
+
+"""
 
 
 
 
 from typing import List,Union,Optional
 
+
+
 def calculate_area_rectangle(h:int, w:int)->int:
+    """
+    this function takes in h and width representing the height and width of the rectangle in integer 
+    and it returns the area of the rectangle in integer
+
+
+    """
 
     return h*w
 
@@ -20,15 +36,24 @@ numbers :List[int] = ["dd",2,3,4]
 print(numbers)
 
 
-def a_function(a: Union [int,str]):
-
+def a_function(a: Union [int,str])-> Union [int, str]:
+        """
+        this function takes in a parameter of integer or string and then  returns integer or string
+        
+        """
         print(a)
 
 a_function("sdjnd")
 
 #if could be integer or none 
 
-def a_function_none(a: Optional[int]):
+def a_function_none(a: Optional[int])->Optional[int]:
+      """
+      this function takes in integer or none and returns integer or none 
+      
+      """
       print(a)
+
+
 
 a_function_none(2323)
