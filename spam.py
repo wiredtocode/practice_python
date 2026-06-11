11b1c42a074acf23866c9a68cd2feab2555c158f
Arminder <armincheema@gmail.com>
2026-06-12 00:44:29 +0800

    data_prepping

---
+++ spam.py
@@ -0,0 +1,17 @@
+#we are learning desision tree
+
+#ID3 algorithm , iterative dichotomizer 3   , find the best question to arrange a data
+
+#to reduce entropy
+
+
+import pandas as pd
+
+
+dataset= pd.read_csv('C:\\Users\\armin\\Desktop\\whatevr\\buy.csv')
+
+
+print(dataset)
+
+
+#i copied csv data using pandas and then print it in terminal
